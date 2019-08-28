15d24b1557e7ddd0135e009b62d4cd0237edda2d
Thiago Zaranza <thiagozaranza@gmail.com>
2019-08-27 22:01:17 -0300

    get teams

--- index.js
+++ index.js
@@ -5,6 +5,12 @@ app.get('/', function (req, res) {
   res.send('Hello World!');
 });
 
+app.get('/api/teams', function (req, res) {
+  res
+    .set('Access-Control-Allow-Origin', '*')
+    .send([{id: 1, name: "Thiago"}]);
+});
+
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
 });
